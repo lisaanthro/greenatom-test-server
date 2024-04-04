e2fa47a1b94013c5db1de1e53ef7b5bb1f3916d2
lisaanthro <liza_vater@yahoo.com>
2024-04-04 12:12:54 +0300

    project structure modified

--- routers.py
+++ routers.py
@@ -1,11 +1,9 @@
-from typing import Type, List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from dependencies import get_db
-from crud import add_entry, get_all_entries
-from models import Entry
+from crud.robot import add_entry, get_all_entries
 
-from robot import async_robot
+from robot_script import async_robot
 
 robot_router = APIRouter(
     prefix="/robot",
@@ -21,8 +19,8 @@ async def start_robot(start_id: int) -> dict:
 
 @robot_router.post("/stop")
 async def stop_robot(db: Session = Depends(get_db)) -> dict:
-    await async_robot.stop()
     add_entry(db, async_robot.start_id, async_robot.start_time)
+    await async_robot.stop()
     return {"info": "Robot stopped!"}
 
 
